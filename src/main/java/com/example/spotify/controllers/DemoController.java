1cc7c16a4995e6ed57722fe51e36395bae79c00b
Sadegh Abedi <sadeghabedi@live.com>
2020-07-10 22:08:32 +0430

    add a demo api

--- src/main/java/com/example/spotify/controllers/DemoController.java
+++ src/main/java/com/example/spotify/controllers/DemoController.java
@@ -1,8 +1,31 @@
 package com.example.spotify.controllers;
 
 
+import com.example.spotify.Customer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class DemoController {
+
+    final
+    JdbcTemplate jdbcTemplate;
+
+    public DemoController(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @GetMapping("/rest")
+    public List<Customer> greet(@RequestParam(required = false,
+            defaultValue = "Josh") String name) {
+        return jdbcTemplate.query(
+                "SELECT id, first_name, last_name FROM customers WHERE first_name = ?", new Object[]{name},
+                (rs, rowNum) -> new Customer(rs.getLong("id"), rs.getString("first_name"), rs.getString("last_name"))
+        );
+    }
 }
