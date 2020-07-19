cebdbd24e0dbbdd1598b590f0bac71781559d1e6
Sadegh Abedi <sadeghabedi@live.com>
2020-07-19 21:49:21 +0430

    delete for album and song

--- src/main/java/com/example/spotify/controllers/AlbumController.java
+++ src/main/java/com/example/spotify/controllers/AlbumController.java
@@ -25,25 +25,32 @@ public class AlbumController {
     @PostMapping("/addAlbum")
     public void addAlbum(@RequestParam String token, @RequestBody Album album) {
         var userName = jdbcTemplate.query("SELECT userName FROM token WHERE token =?",
-                new Object[]{token},((rs,rows) -> rs.getString("userName")));
+                new Object[]{token}, ((rs, rows) -> rs.getString("userName")));
 
         String sql = "INSERT INTO album (title,genre,artist,publishDate,songs) VALUES (?,?,?,?,?)";
-
-        jdbcTemplate.update(sql, album.getTitle(),album.getGenre(),userName, new Date(System.currentTimeMillis()),album.getSongs());
+        album.getSongs().forEach((song) -> jdbcTemplate.update(
+                sql,
+                album.getTitle(),
+                album.getGenre(),
+                userName,
+                new Date(System.currentTimeMillis()), album.getSongs()));
 
     }
 
     @DeleteMapping("/removeAlbum")
-    public void removeAlbum(@RequestBody Album album){
+    public void removeAlbum(@RequestBody Album album) {
         //Just use id of album
         String sql = "DELETE FROM album WHERE id=?";
-        jdbcTemplate.update(sql,album.getId());
+        jdbcTemplate.update(sql, album.getId());
+        sql = "DELETE FROM song WHERE albumId=?";
+        jdbcTemplate.update(sql, album.getId());
 
     }
 
     @DeleteMapping("/removeSongs")
-    public void removeSongs(@RequestParam String token,@RequestBody Album album){
+    public void removeSongs(@RequestParam String token, @RequestBody Album album) {
         //just delete songs in the songsList in album
+        album.getSongs().forEach((song -> jdbcTemplate.update("DELETE FROM song WHERE id=?", song.getId())));
     }
 
 }
